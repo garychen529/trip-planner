b4a829d3ca35833d60a430fd65a81a766e729d19
Joseph Bona <joseph.bona07@gmail.com>
2016-08-23 20:50:32 -0400

    front-end layout

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ app.use(bodyParser.json());
 app.use(volleyball);
 
 app.use(express.static(__dirname + '/public'));
+app.use('/bootstrap', express.static(__dirname + '/bower_components/bootstrap/dist'))
+app.use('/jquery', express.static(__dirname + '/bower_components/jquery/dist'))
 
 app.use(function(err, req, res, next) {
 	console.log("Oh noes!!!!!");
